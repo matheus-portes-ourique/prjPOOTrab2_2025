735665e1c3d92954dcd98e43813a849a18bf1571
Erixian <cibercianocode@gmail.com>
2025-11-25 15:54:52 -0300

    feat: metodo 'alterar' e 'deletar' para paciente

--- POOTrab1_2025_3/src/fatec/poo/control/DaoPaciente.java
+++ POOTrab1_2025_3/src/fatec/poo/control/DaoPaciente.java
@@ -49,12 +49,10 @@ public class DaoPaciente {
                                        "where cpf = ?");
             
             ps.setString(1, cpf);
-            ResultSet rs = ps.executeQuery();
-            
-            if(rs.next()){
-                DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-                
-                p = new Paciente(cpf, rs.getString("nome"), LocalDate.parse(rs.getString("dataNascimento")));
+            ResultSet rs = ps.executeQuery(); 
+            if(rs.next()){ 
+                p = new Paciente(cpf, rs.getString("nome"), 
+                                LocalDate.parse(rs.getString("dataNascimento")));
             }
             
         }catch(SQLException ex){
@@ -64,11 +62,38 @@ public class DaoPaciente {
         return p;
     }
     
+    //FIQUEI NA DUVIDA SE O METODO TEM QUE RECEBER UM OBJ PACIENTE
+    //OU APENAS O CPF
+    //ALTEREI APENAS OS ATRIBUTOS SETTAVEIS DA CLASSE PACIENTE/PESSOA
     public void alterar(Paciente paciente){
-        
+        PreparedStatement ps = null;
+        String sql = "UPDATE tb_paciente SET altura = ?, " 
+                   + "peso = ?, "
+                   + "endereco = ?, "
+                   + "telefone = ?, "
+                   + "WHERE cpf = ?;";
+        try{
+            ps = conn.prepareStatement(sql);
+            ps.setDouble(1, paciente.getAltura());
+            ps.setDouble(2, paciente.getPeso());
+            ps.setString(3, paciente.getCpf());
+            ps.setString(4, paciente.getEndereco());
+            ps.setString(5, paciente.getTelefone());
+            ps.execute();
+        } catch(SQLException ex) {
+            System.out.println(ex.toString());
+        }
     }
     
+    //DELECAO COM BASE NO CPF
     public void excluir(Paciente paciente){
-        
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement("DELETE FROM tb_paciente WHERE cpf = ?;");
+            ps.setString(1, paciente.getCpf());
+            ps.execute();
+        } catch(SQLException ex) {
+            System.out.println(ex.toString());
+        }
     }
 }
