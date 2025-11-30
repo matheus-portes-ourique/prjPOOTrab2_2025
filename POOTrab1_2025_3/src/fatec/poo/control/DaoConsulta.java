ed85702ad13362ebfcad721498ff97a5a53bbc46
Evandro Rodrigues de Arruda <evandro.rodrigues_arruda@hotmail.com>
2025-11-30 14:19:56 -0300

    Finalização da classe DaoConsulta

--- POOTrab1_2025_3/src/fatec/poo/control/DaoConsulta.java
+++ POOTrab1_2025_3/src/fatec/poo/control/DaoConsulta.java
@@ -1,4 +1,3 @@
-
 package fatec.poo.control;
 
 import java.sql.Connection;
@@ -7,20 +6,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import fatec.poo.model.Consulta;
+import fatec.poo.model.Medico;
+import fatec.poo.control.DaoMedico;
+
 /**
  * @author 0030482411017
  */
 
 public class DaoConsulta {
+
     public Connection conn;
 
     public DaoConsulta(Connection conn) {
         this.conn = conn;
     }
-    
-    public void inserir(Consulta consulta){
+
+    public void inserir(Consulta consulta) {
         PreparedStatement ps = null;
-        
+
         try {
             ps = conn.prepareStatement("INSERT INTO tb_consulta VALUES (?,?,?,?)");
             ps.setInt(1, consulta.getCodigo());
@@ -28,71 +31,68 @@ public class DaoConsulta {
             ps.setDouble(3, consulta.getValor());
             ps.setString(4, consulta.getMedico().getCpf());
             ps.execute();
-            
-        } catch (SQLException ex){
+
+        } catch (SQLException ex) {
             System.out.println(ex.toString());
         }
     }
-    
-    public void alterar(Consulta consulta){
+
+    public void alterar(Consulta consulta) {
         PreparedStatement ps = null;
-        
+
         try {
             ps = conn.prepareStatement("UPDATE tb_consulta SET valor = ?,"
-                                                     + "med_cpf = ?"
-                                                     + "WHERE codigo = ?");
+                    + "med_cpf = ?"
+                    + "WHERE codigo = ?");
             ps.setDouble(1, consulta.getValor());
             ps.setObject(2, consulta.getMedico());
             ps.setInt(3, consulta.getCodigo());
-            
+
             ps.execute();
         } catch (SQLException ex) {
             System.out.println(ex.toString());
         }
-    
+
     }
-    
-    public Consulta consultar(int codigo){
+
+    public Consulta consultar(int codigo) {
         PreparedStatement ps = null;
         Consulta c = null;
-        
+
         try {
             ps = conn.prepareStatement("SELECT * FROM tb_consultas"
-                                     + "WHERE codigo = ?");
-            
+                    + "WHERE codigo = ?");
             ps.setInt(1, codigo);
             ResultSet rs = ps.executeQuery();
-            
-            if(rs.next()){
+
+            if (rs.next()) {
                 c = new Consulta(rs.getInt("codigo"), rs.getString("cons_data"));
                 c.setValor(rs.getDouble("valor"));
                 
-                //não imagino como alterar o medico por ele ser um objeto. eu vejo isos amanhã.
-                
+                DaoMedico dm = new DaoMedico(conn);
+                Medico m = dm.consultar(rs.getString("med_cpf"));
+                c.setMedico(m);
             }
-                
         } catch (SQLException ex) {
             System.out.println(ex.toString());
         }
         return c;
-        
     }
-    
-    public void excluir(Consulta consulta){
+
+    public void excluir(Consulta consulta) {
         PreparedStatement ps = null;
-        
+
         try {
             ps = conn.prepareStatement("DELETE FROM tb_consulta "
-                                     + "WHERE codigo = ?");
-            
+                    + "WHERE codigo = ?");
+
             ps.setInt(1, consulta.getCodigo());
             ps.execute();
-            
+
         } catch (SQLException ex) {
             System.out.println(ex.toString());
         }
-        
+
     }
-    
-    
+
 }
